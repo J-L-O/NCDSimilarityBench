cd65986e609ceed358c4718cb845c5ebbf227a0f
J-L-O <18723283+J-L-O@users.noreply.github.com>
2023-05-16 18:25:46 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # NCDSimilarityBench
+
+This repository contains a standalone implementation of our semantic similarity benchmark 
+proposed in our TMLR paper 
+
+> **Supervised Knowledge May Hurt Novel Class Discovery Performance**<br>
+> Ziyun Li, Jona Otholt, Ben Dai, Di Hu, Christoph Meinel, Haojin Yang
+
+Our main repository for the paper can be found [here](https://github.com/J-L-O/SK-Hurt-NCD).
+The benchmark is based on the ImageNet dataset [1] and the Entity30 subpopulation shift task 
+introduced in [2].
+It consists of multiple labeled and unlabeled datasets with varying degrees of semantic 
+similarity between them.
+
+<div style="text-align: center;">
+  <img src="./images/benchmark.png" alt="Figure depicting the general principle of our 
+benchmark. For more details, see Section 4 of our paper.">
+</div>
+
+By combining different labeled and unlabeled datasets, we can create NCD tasks that, while 
+sharing the number of labeled and unlabeled classes, differ in the semantic similarity 
+between them.
+This enables researchers to evaluate their methods under varying degrees of semantic
+similarity.
+This standalone package allows to easily use the benchmark for your own
+experiments.
+
+## Installation
+
+To install the package, simply run
+
+```bash
+pip install git+https://github.com/J-L-O/NCDSimilarityBench.git
+```
+
+The package requires Python 3.7 or higher and depends on numpy and torchvision.
+
+## References
+
+[1] Deng, Jia, et al. "Imagenet: A large-scale hierarchical image database." 2009 IEEE conference on computer vision and pattern recognition. Ieee, 2009.
+
+[2] Santurkar, Shibani, Dimitris Tsipras, and Aleksander Madry. "BREEDS: Benchmarks for Subpopulation Shift." International Conference on Learning Representations. 2021.
+
+[3] Vaze, Sagar, et al. "Generalized category discovery." Proceedings of the IEEE/CVF Conference on Computer Vision and Pattern Recognition. 2022.
---
+++ src/similaritybench/datasets.py
@@ -0,0 +1,72 @@
+from typing import Tuple, List, Dict
+from importlib_resources import files
+
+import numpy as np
+from importlib_resources.abc import Traversable
+from torchvision.datasets import ImageNet
+
+class DiscoverTargetTransform:
+    def __init__(self, mapping):
+        self.mapping = mapping
+
+    def __call__(self, y):
+        y = self.mapping[y]
+        return y
+
+class ImageNetSplit(ImageNet):
+    LABELED_SPLITS = ["l1", "l1.5", "l2"]
+    UNLABELED_SPLITS = ["u1", "u2"]
+
+    def __init__(
+            self,
+            root: str,
+            split: str = "train",
+            labeled_split: str = "L1",
+            unlabeled_split: str = "U1",
+            **kwargs
+    ):
+        labeled_lower = labeled_split.lower()
+        unlabeled_lower = unlabeled_split.lower()
+
+        assert labeled_lower in self.LABELED_SPLITS, f"Labeled split {labeled_split} is not supported!"
+        assert unlabeled_lower in self.UNLABELED_SPLITS, f"Unlabeled split {unlabeled_split} is not supported!"
+
+        self.labeled_split = labeled_lower
+        self.unlabeled_split = unlabeled_lower
+        super(ImageNetSplit, self).__init__(root, split, **kwargs)
+
+    @staticmethod
+    def _get_class_list_path(is_labeled: bool, split: str) -> Traversable:
+        labeled = "labeled" if is_labeled else "unlabeled"
+        file_name = f"imagenet_{labeled}_{split}.txt"
+        return files("similaritybench.splits").joinpath(file_name)
+
+    def _initialize_entity30(self, class_to_idx):
+        labeled_path = self._get_class_list_path(True, self.labeled_split)
+        unlabeled_path = self._get_class_list_path(False, self.unlabeled_split)
+
+        labeled_classes = list(np.loadtxt(labeled_path, dtype=str))
+        unlabeled_classes = list(np.loadtxt(unlabeled_path, dtype=str))
+
+        labeled_class_idxs = np.array([class_to_idx[name] for name in labeled_classes])
+        unlabeled_class_idxs = np.array([class_to_idx[name] for name in unlabeled_classes])
+
+        # target transform
+        all_class_idxs = np.concatenate((labeled_class_idxs, unlabeled_class_idxs))
+
+        target_transform = DiscoverTargetTransform(
+            {original: target for target, original in enumerate(all_class_idxs)}
+        )
+        self.target_transform = target_transform
+
+        new_classes = labeled_classes + unlabeled_classes
+        new_class_to_idx = {cls: idx for cls, idx in zip(new_classes, all_class_idxs)}
+
+        return new_classes, new_class_to_idx
+
+    def _find_classes(self, dir: str) -> Tuple[List[str], Dict[str, int]]:
+        classes, class_to_idx = super(ImageNetSplit, self)._find_classes(dir)
+
+        subset_classes, subset_class_to_idx = self._initialize_entity30(class_to_idx)
+
+        return subset_classes, subset_class_to_idx
